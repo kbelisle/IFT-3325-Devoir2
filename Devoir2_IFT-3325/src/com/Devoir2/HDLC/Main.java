b531544674bc627f14bcc014986821a0469f747b
kbelisle <belisle57@msn.com>
2018-11-22 13:10:47 -0800

    Fix equals in main

--- Devoir2_IFT-3325/src/com/Devoir2/HDLC/Main.java
+++ Devoir2_IFT-3325/src/com/Devoir2/HDLC/Main.java
@@ -5,23 +5,25 @@ public class Main {
 		if(args.length < 1)
 			System.out.println("Please enter arguments");
 		
-		if(args[0] == "Sender" && args.length == 5) {
-			if(args[4] != "0") {
+		if(args[0].compareTo("Sender") == 0 && args.length == 5) {
+			if(args[4].compareTo("0") != 0) {
 				System.out.println("Only supports Go-Back-N");
 				return;
 			}
 			try {
 				int port = Integer.parseInt(args[2]);
 				new Sender(args[1],port,args[3]);
+				System.out.println("Sender process ended.");
 			}
 			catch(Exception e) {
 				System.out.println(e.getMessage());
 			}
 		}
-		else if(args[0] == "Receiver" && args.length==2) {
+		else if(args[0].compareTo("Receiver") == 0 && args.length==2) {
 			try {
 				int port = Integer.parseInt(args[1]);
 				new Receiver(port);
+				System.out.println("Receiver process ended.");
 			}
 			catch(Exception e) {
 				System.out.println(e.getMessage());
