4871022a9decf8dfb2c9bb2f45f808c88e64f392
Olivierbelec <43650111+Olivierbelec@users.noreply.github.com>
2018-11-26 14:15:41 -0500

    Update Receiver.java
    Validity is given to waitForFrame

--- Devoir2_IFT-3325/src/com/Devoir2/HDLC/Receiver.java
+++ Devoir2_IFT-3325/src/com/Devoir2/HDLC/Receiver.java
@@ -10,6 +10,7 @@ public class Receiver {
 	int next;
 	long timer;
 	String txt="";
+	
 	/*socket*/
 	ServerSocket server;
 	Socket socket;
@@ -17,67 +18,71 @@ public class Receiver {
 	DataOutputStream output;
 	
 	/*Constructors*/
-	public Receiver(int port) throws Exception {
+	public Receiver(int port) {
 		// To cycle through 0-7. Next+7%8 should tell which value shouldn't appear.
 		next=0;
 		
 		// We need an arg to know the port. Need additionnal condition to know it's an int between some numbers
-		/*Connect to socket*/
-		server = new ServerSocket(port);
-        socket = server.accept(); 
-        input = new DataInputStream(socket.getInputStream());
-        output = new DataOutputStream(socket.getOutputStream());
+		try {/*Connect to socket*/
+			server = new ServerSocket(port);
+	        socket = server.accept(); 
+	        input = new DataInputStream(socket.getInputStream());
+	        output = new DataOutputStream(socket.getOutputStream());
+
+	        // Simple logic loop
+	        while(true) {
+	        	Frame frame =waitForFrame();
+	        	String message=frame.getMessage();
+	        	System.out.println("Received : " + message);
+	        	if("F0".equals(message))
+	        	    break;
+	        	if("C0".equals(message)&&lastMsg==null) {
+	        		lastMsg=new Frame("A0",true);
+	        		output.writeUTF(lastMsg.getFrame());
+	    			timer=(new Date()).getTime();
+	        		System.out.println("Sent : " + lastMsg.getMessage());
+	        	}
+	        	if(lastMsg!=null) {
+	        		if("P0".equals(message)) {
+	        			lastMsg=new Frame("A"+next,true);
+		        		output.writeUTF(lastMsg.getFrame());
+		    			timer=(new Date()).getTime();
+		        		System.out.println("Sent : " + lastMsg.getMessage());	
+		        	}
+	        		if(message.length()>=2&&("I").equals(message.substring(0, 1))) {
+		        		if((""+next).equals(message.substring(1, 2))){
+		        		    txt+=message.substring(2);//save
+		        		    lastMsg = new Frame("A"+next,true);
+		        		    next=(next+1)%8;
+		        		}else{
+		        			lastMsg = new Frame("R"+next,true);
+		        		}
+		        		output.writeUTF(lastMsg.getFrame());
+		    			timer=(new Date()).getTime();
+		        		System.out.println("Sent : " + lastMsg.getMessage());		
+		        	}
+	        	}
+	        }
+	        
+	        // Save file
+	        File file = new File("Data.txt");
+	        BufferedWriter out = new BufferedWriter(new FileWriter(file)); 
+	        out.write(txt);
+	        out.close();
+	        
+	        // Releases ressources
+	        input.close();
+	        output.close();
+	        socket.close();
+	        server.close();
+		} catch (NumberFormatException e) {
+			return;
+		} catch (IOException e) {
+			return;
+		}
 	}
 	/*Methods*/
-	public void listen() throws Exception {
-		// Simple logic loop
-        while(true) {
-        	Frame frame =waitForFrame();
-        	String message=frame.getMessage();
-        	System.out.println("Received : " + message);
-        	if("F0".equals(message))
-        	    break;
-        	if("C0".equals(message)&&lastMsg==null) {
-        		lastMsg=new Frame("A0",true);
-        		output.writeUTF(lastMsg.getFrame());
-    			timer=(new Date()).getTime();
-        		System.out.println("Sent : " + lastMsg.getMessage());
-        	}
-        	if(lastMsg!=null) {
-        		if("P0".equals(message)) {
-        			lastMsg=new Frame("A"+next,true);
-	        		output.writeUTF(lastMsg.getFrame());
-	    			timer=(new Date()).getTime();
-	        		System.out.println("Sent : " + lastMsg.getMessage());	
-	        	}
-        		if(message.length()>=2&&("I").equals(message.substring(0, 1))) {
-	        		if((""+next).equals(message.substring(1, 2))){
-	        		    txt+=message.substring(2);//save
-	        		    lastMsg = new Frame("A"+next,true);
-	        		    next=(next+1)%8;
-	        		}else{
-	        			lastMsg = new Frame("R"+next,true);
-	        		}
-	        		output.writeUTF(lastMsg.getFrame());
-	    			timer=(new Date()).getTime();
-	        		System.out.println("Sent : " + lastMsg.getMessage());		
-	        	}
-        	}
-        }
-        
-        // Save file
-        File file = new File("Data.txt");
-        BufferedWriter out = new BufferedWriter(new FileWriter(file)); 
-        out.write(txt);
-        out.close();
-        
-        // Releases ressources
-        input.close();
-        output.close();
-        socket.close();
-        server.close();
-	}
-	private Frame waitForFrame() {
+	public Frame waitForFrame() {
 		try {
 			while(true) {
 				while(input.available()<1) {
