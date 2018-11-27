cd0b506f5695dfacb5da7bf5538e41f71fde7439
root <root@ubuntu>
2018-11-27 10:08:12 -0800

    Modif test Receiver, ajout logic multi-thread

--- Devoir2_IFT-3325/src/com/Devoir2/HDLC/Receiver.java
+++ Devoir2_IFT-3325/src/com/Devoir2/HDLC/Receiver.java
@@ -25,59 +25,60 @@ public class Receiver {
 		// We need an arg to know the port. Need additionnal condition to know it's an int between some numbers
 		/*Connect to socket*/
 		server = new ServerSocket(port);
-        socket = server.accept(); 
-        input = new DataInputStream(socket.getInputStream());
-        output = new DataOutputStream(socket.getOutputStream());
 	}
 	/*Methods*/
 	public void listen() throws Exception {
-	        // Core loop
-	        while(true) {
-	        	Frame frame =waitForFrame();
-	        	String message=frame.getMessage();
-	        	System.out.println("Received : " + message);
-	        	if("F0".equals(message))
-	        	    break;
-	        	if("C0".equals(message)){//&&lastMsg==null) {
-	        		next=0;
-	        		lastMsg=new Frame("A0",true);
+		/*server.accept() blocks until new socket connection*/
+        socket = server.accept(); 
+        input = new DataInputStream(socket.getInputStream());
+        output = new DataOutputStream(socket.getOutputStream());
+        // Core loop
+        while(true) {
+        	Frame frame =waitForFrame();
+        	String message=frame.getMessage();
+        	System.out.println("Received : " + message);
+        	if("F0".equals(message))
+        	    break;
+        	if("C0".equals(message)){//&&lastMsg==null) {
+        		next=0;
+        		lastMsg=new Frame("A0",true);
+        		output.writeUTF(lastMsg.getFrame());
+    			timer=(new Date()).getTime();
+        		System.out.println("Sent : " + lastMsg.getMessage());
+        	}
+        	if(lastMsg!=null) {
+        		if("P0".equals(message)) {
+        			lastMsg=new Frame("A"+next,true);
 	        		output.writeUTF(lastMsg.getFrame());
 	    			timer=(new Date()).getTime();
-	        		System.out.println("Sent : " + lastMsg.getMessage());
+	        		System.out.println("Sent : " + lastMsg.getMessage());	
 	        	}
-	        	if(lastMsg!=null) {
-	        		if("P0".equals(message)) {
-	        			lastMsg=new Frame("A"+next,true);
-		        		output.writeUTF(lastMsg.getFrame());
-		    			timer=(new Date()).getTime();
-		        		System.out.println("Sent : " + lastMsg.getMessage());	
-		        	}
-	        		if(message.length()>=2&&("I").equals(message.substring(0, 1))) {
-		        		if((""+next).equals(message.substring(1, 2))){
-		        		    txt+=message.substring(2);//save
-		        		    next=(next+1)%8;
-		        		    lastMsg = new Frame("A"+next,true);
-		        		}else{
-		        			lastMsg = new Frame("R"+next,true);
-		        		}
-		        		output.writeUTF(lastMsg.getFrame());
-		    			timer=(new Date()).getTime();
-		        		System.out.println("Sent : " + lastMsg.getMessage());		
-		        	}
+        		if(message.length()>=2&&("I").equals(message.substring(0, 1))) {
+	        		if((""+next).equals(message.substring(1, 2))){
+	        		    txt+=message.substring(2);//save
+	        		    next=(next+1)%8;
+	        		    lastMsg = new Frame("A"+next,true);
+	        		}else{
+	        			lastMsg = new Frame("R"+next,true);
+	        		}
+	        		output.writeUTF(lastMsg.getFrame());
+	    			timer=(new Date()).getTime();
+	        		System.out.println("Sent : " + lastMsg.getMessage());		
 	        	}
-	        }
-	        
-	        // Save file
-	        File file = new File("Data.txt");
-	        BufferedWriter out = new BufferedWriter(new FileWriter(file)); 
-	        out.write(txt);
-	        out.close();
-	        
-	        // Releases ressources
-	        input.close();
-	        output.close();
-	        socket.close();
-	        server.close();
+        	}
+        }
+        
+        // Save file
+        File file = new File("Data.txt");
+        BufferedWriter out = new BufferedWriter(new FileWriter(file)); 
+        out.write(txt);
+        out.close();
+        
+        // Releases ressources
+        input.close();
+        output.close();
+        socket.close();
+        server.close();
 	}
 	
 	private Frame waitForFrame() {
--- Devoir2_IFT-3325/src/com/Devoir2/Test/Scenario.java
+++ Devoir2_IFT-3325/src/com/Devoir2/Test/Scenario.java
@@ -2,16 +2,22 @@ package com.Devoir2.Test;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import com.Devoir2.HDLC.*;
 
 public class Scenario {
 	public static void main(String[] args) {
-		
-		//Frame Unit test
-		
+		testFrame();
+		testReceiver();
+	}
+	
+	private static void testFrame() {
+		//Frame Unit test	
 		System.out.print("Test 1, no flags: ");
 		System.out.println(new Frame("0",false).isValid()?"X":"Success");
 		
@@ -41,9 +47,10 @@ public class Scenario {
 		
 		System.out.print("Test 10, bit stuffing: ");
 		System.out.println("01111110001111101001111101110100100001011101111110".equals(new Frame("??",true).getFrame())?"Success":"X");
-		
+	}
+	
+	private static void testReceiver() {
 		//Receiver Unit test
-		
 		String C0=new Frame("C0",true).getFrame(),
 				I0=new Frame("I0X",true).getFrame(),
 				I1=new Frame("I1",true).getFrame(),
@@ -58,29 +65,141 @@ public class Scenario {
 				{"Repetition",C0,I0,I0,I0,I0,F0},
 				{"Timeout",C0,I0,"w",I0,"w",I1,"w",F0},
 				{"P",C0,I0,I0,P0,I1,F0}
-				};
+				};	
+		ArrayList<String> results = new ArrayList<String>();
+		int testNumber = 11;
+		for(String[] s : liste) {
+			results.add("Test #" + testNumber + " " + s[0] + " : " + testReceiverWithCommand(s,testNumber));
+			testNumber++;
+
+		}
+		/*Allow the console to catch-up*/
 		try {
-			Socket s = new Socket("127.0.0.1",5000);
+			TimeUnit.SECONDS.sleep(3);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("\n\n\n");
+		for(String res : results) {
+			System.out.println(res);
+		}
+	}
+	
+	private static String testReceiverWithCommand(String[] command, int testNumber) {
+		try {
+			/*Start a thread for the receiver*/
+			Thread tr = new Thread() {
+				public void run() {
+					try {
+						new Receiver(8750 + testNumber).listen();
+					}
+					catch (Exception e) {
+						System.out.println("Receiver thread wasnt able to start : " + e.getMessage());
+					}
+				}
+			};
+			tr.start();
+			/*Wait for server to start*/
+			Thread.sleep(1000);
+			Socket s = new Socket("localhost",8750 + testNumber);
 			DataInputStream in = new DataInputStream(s.getInputStream());
 		    DataOutputStream out = new DataOutputStream(s.getOutputStream());
-		    for(int i=0;i<liste.length;i++) {
-		    	System.out.print("Test "+1+i+", "+liste[i][0]+": ");
-				for(int j=1;j<liste[i].length;j++) {
-					if(!"w".equals(liste[i][j]))
-						out.writeUTF(liste[i][j]);
-		            long timer=new Date().getTime();
-		            while(in.available()<1&&new Date().getTime()-timer<3000) {}
-		            if(in.available()>=1)
-		            	System.out.println(new Frame(in.readUTF(),false).getMessage());
-				}
-				System.out.println("Success");
+	    	System.out.print("Test "+testNumber+", "+command[0]+": ");
+			for(int j=1;j<command.length;j++) {
+				if(!"w".equals(command[j]))
+					out.writeUTF(command[j]);
+	            long timer=new Date().getTime();
+	            while(in.available()<1&&new Date().getTime()-timer<3000) {}
+	            if(in.available()>=1)
+	            	System.out.println("Received from Receiver : " + new Frame(in.readUTF(),false).getMessage());
 			}
 		    out.writeUTF(new Frame("F0",true).getFrame());
 		    out.close();
 		    in.close();
 		    s.close();
+		    return "Success";
 		} catch (Exception e) {
-			System.out.println("Failed");
+			return "Failed : " + e.getMessage();
 		}
 	}
+	
+	/*public static void testSender() {
+		//Sender Unit test
+		String C0=new Frame("C0",true).getFrame(),
+				I0=new Frame("I0X",true).getFrame(),
+				I1=new Frame("I1",true).getFrame(),
+				A0=new Frame("A0",true).getFrame(),
+				P0=new Frame("P0",true).getFrame(),
+				F0=new Frame("F1",true).getFrame();
+		String[][] liste= {
+				{"No connection",I0,I0,F0},
+				{"Noise",A0,C0,I0,I1,F0,A0},
+				{"Corruption",C0,A0,F0},
+				{"Out of order",C0,I1,I1,F0},
+				{"Repetition",C0,I0,I0,I0,I0,F0},
+				{"Timeout",C0,I0,"w",I0,"w",I1,"w",F0},
+				{"P",C0,I0,I0,P0,I1,F0}
+				};	
+		ArrayList<String> results = new ArrayList<String>();
+		int testNumber = 11;
+		for(String[] s : liste) {
+			results.add("Test #" + testNumber + " " + s[0] + " : " + testReceiverWithCommand(s,testNumber));
+			testNumber++;
+
+		}
+		//Allow the console to catch-up
+		try {
+			TimeUnit.SECONDS.sleep(3);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("\n\n\n");
+		for(String res : results) {
+			System.out.println(res);
+		}
+	}
+	
+	private static String testSenderWithCommand(String[] command, int testNumber) {
+		try {
+			ServerSocket server = new ServerSocket(7750 + testNumber);	
+			//Start a thread for the sender
+			Thread tr = new Thread() {
+				public void run() {
+					try {
+						Sender s = new Sender("localhost",7750 + testNumber,"");
+					}
+					catch (Exception e) {
+						System.out.println("Receiver thread wasnt able to start : " + e.getMessage());
+					}
+				}
+			};
+			tr.start();
+			//Wait for server to start
+			server.accept();
+			Socket s = new Socket("localhost",8750 + testNumber);
+			DataInputStream in = new DataInputStream(s.getInputStream());
+		    DataOutputStream out = new DataOutputStream(s.getOutputStream());
+		    
+	    	System.out.print("Test "+testNumber+", "+command[0]+": ");
+			for(int j=1;j<command.length;j++) {
+				if(!"w".equals(command[j]))
+					out.writeUTF(command[j]);
+	            long timer=new Date().getTime();
+	            while(in.available()<1&&new Date().getTime()-timer<3000) {}
+	            if(in.available()>=1)
+	            	System.out.println("Received from Receiver : " + new Frame(in.readUTF(),false).getMessage());
+			}
+		    out.writeUTF(new Frame("F0",true).getFrame());
+		    out.close();
+		    in.close();
+		    s.close();
+		    return "Success";
+		} catch (Exception e) {
+			return "Failed : " + e.getMessage();
+		}
+	}*/
 }
