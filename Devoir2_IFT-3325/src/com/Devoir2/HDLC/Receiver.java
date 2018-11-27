6246a915a5c5691af396f27ad73d694af1840458
Olivierbelec <43650111+Olivierbelec@users.noreply.github.com>
2018-11-26 22:15:11 -0500

    Update Receiver.java

--- Devoir2_IFT-3325/src/com/Devoir2/HDLC/Receiver.java
+++ Devoir2_IFT-3325/src/com/Devoir2/HDLC/Receiver.java
@@ -31,37 +31,38 @@ public class Receiver {
 	}
 	/*Methods*/
 	public void listen() throws Exception {
-	        // Simple logic loop
+	        // Core loop
 	        while(true) {
 	        	Frame frame =waitForFrame();
 	        	String message=frame.getMessage();
 	        	System.out.println("Received : " + message);
 	        	if("F0".equals(message))
 	        	    break;
-	        	if("C0".equals(message)&&lastMsg==null) {
+	        	if("C0".equals(message)){//&&lastMsg==null) {
+	        		next=0;
 	        		lastMsg=new Frame("A0",true);
 	        		output.writeUTF(lastMsg.getFrame());
 	    			timer=(new Date()).getTime();
-	        		System.out.println("Sent : " + lastMsg.getMessage());
+	        		//System.out.println("Sent : " + lastMsg.getMessage());
 	        	}
 	        	if(lastMsg!=null) {
 	        		if("P0".equals(message)) {
 	        			lastMsg=new Frame("A"+next,true);
 		        		output.writeUTF(lastMsg.getFrame());
 		    			timer=(new Date()).getTime();
-		        		System.out.println("Sent : " + lastMsg.getMessage());	
+		        		//System.out.println("Sent : " + lastMsg.getMessage());	
 		        	}
 	        		if(message.length()>=2&&("I").equals(message.substring(0, 1))) {
 		        		if((""+next).equals(message.substring(1, 2))){
 		        		    txt+=message.substring(2);//save
-		        		    lastMsg = new Frame("A"+next,true);
 		        		    next=(next+1)%8;
+		        		    lastMsg = new Frame("A"+next,true);
 		        		}else{
 		        			lastMsg = new Frame("R"+next,true);
 		        		}
 		        		output.writeUTF(lastMsg.getFrame());
 		    			timer=(new Date()).getTime();
-		        		System.out.println("Sent : " + lastMsg.getMessage());		
+		        		//System.out.println("Sent : " + lastMsg.getMessage());		
 		        	}
 	        	}
 	        }
